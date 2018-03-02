2bbe117a27b0176ae2c9f552f7cdd01fe23d38e2
sanjitpush <35615406+sanjitpush@users.noreply.github.com>
2018-03-02 10:37:16 -0500

    Update lab04.c

--- lab04.c
+++ lab04.c
@@ -10,15 +10,18 @@ void main() {
         srand(time(NULL));
 
         for(int i = 0; i < 1000; i++){
+		//Generate random numbers between -1000 and 1000 (2001 different numbers that can be generated)
                 array1[i] = (rand() % 2001) - 1000;
                 array2[i] = (rand() % 2001) - 1000;
 	}
 
 	for(int i = 0; i < MAX; i++){
+		//Store elements into 3rd array summed up
 		array3[i] = array1[i] + array2[i];
 	}
 
 	for(int i = 0; i < MAX; i++){
+		//Sum up 3rd array
 		sum += array3[i];
 	}
 
